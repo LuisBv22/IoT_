7d58ae395590a49f14f86ead2025e4d4538ada8c
LuisBv22 <luis221101@hotmail.com>
2023-10-11 19:46:51 -0600

    ahora si ya es el bueno bueno

--- pg1_1_20231011.py
+++ pg1_1_20231011.py
@@ -10,6 +10,7 @@ accion = {"Parpadear":True, "Prendido":True}
 
 seleccion = "hola"
 selec = "xd"
+cantidad = 1
 
 # Puerto de los LED
 ledVerde = LED(17)
@@ -25,6 +26,8 @@ while True:
     seleccion = input()
     
     if True == colores[seleccion]:
+        cantidad=0
+        
         print('¿Que quieres hacer?')
         print('1. Parpadear')
         print('2. Prendido')
@@ -33,31 +36,35 @@ while True:
         if seleccion == "Verde":
             if True == accion[selec]:
                 if selec == "Parpadear":
-                    while True:
+                    
+                    while cantidad != 5:
                         ledVerde.on()
                         sleep(1)
                         ledVerde.off()
                         sleep(1)
+                        cantidad=cantidad+1
                 else:
                     ledVerde.on()
         if seleccion == "Azul":
             if True == accion[selec]:
                 if selec == "Parpadear":
-                    while True:
+                    while cantidad != 5:
                         ledAzul.on()
                         sleep(1)
                         ledAzul.off()
                         sleep(1)
+                        cantidad=cantidad+1
                 else:
                     ledAzul.on()
         if seleccion == "Rojo":
             if True == accion[selec]:
                 if selec == "Parpadear":
-                    while True:
+                    while cantidad != 5:
                         ledRojo.on()
                         sleep(1)
                         ledRojo.off()
                         sleep(1)
+                        cantidad=cantidad+1
                 else:
                     ledRojo.on()
                             
