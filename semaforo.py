67fb58c2a121a12a477c680cf702329340f94c0b
ElisaGuerra <eliguesan2904@gmail.com>
2023-10-20 20:35:19 -0600

    Clase_20102023

---
+++ semaforo.py
@@ -0,0 +1,14 @@
+from gpiozero import LED,Button
+from time import sleep
+
+# Puerto de los LED
+ledVerde = LED(17)
+ledAmarillo = LED(27)
+ledRojo = LED(22)
+boton = Button(10)
+
+while True:
+    
+    ledVerde.on()
+           
+        
