dbabfa2325929ccad28d85524d714a83fc14c931
otac12 <otac@hotmail.com>
2023-10-23 15:08:19 -0600

    actualizacion

--- semaforo.py
+++ semaforo.py
@@ -8,7 +8,20 @@ ledRojo = LED(22)
 boton = Button(10)
 
 while True:
+
+    if boton.is_pressed:
+        if ledRojo.is_lit == False  and  ledAmarillo.is_lit == False and ledVerde.is_lit == False:
+             ledVerde.on()
+        
+        if ledVerde.is_lit == True:
+             ledVerde.off()
+             ledAmarillo.on()
     
-    
-           
+        if ledAmarillo.is_lit == True:
+             ledAmarillo.off()
+             ledRojo.on()
+
+        if ledRojo.is_lit == True:
+             ledRojo.off()
+             ledVerde.on()
         
